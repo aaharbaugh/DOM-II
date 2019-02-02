a547a90b4198a2ce9167f7e15abf22c078025ab3
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-02 13:23:59 -0600

    added more event triggers

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ container.addEventListener('wheel', (e) => {
     }
     if (e.deltaY > 0) {
     imgContent.forEach(img => {
-        img.style.width = "50%"
+        img.style.width = "90%"
     })
     }
 })
@@ -53,3 +53,63 @@ topImage.addEventListener('click', (e) => {
     topImage.setAttribute('src', imgArray[count])
     count++
 } )
+
+//change size of top header on click
+let count2 = 0
+const topHeader = document.querySelector('.logo-heading');
+let headerSize = [
+    ['green', '4.5rem'],
+    ['orange', '5rem'],
+    ['purple', '5.5rem'],
+    ['maroon', '6rem']
+]
+
+topHeader.addEventListener('click', (e) => {
+    if(count2 > 3){
+        count2 = 0
+    }
+    e.stopPropagation();
+    topHeader.style.color = headerSize[count2][0]
+    topHeader.style.fontSize = headerSize[count2][1]
+    count2++
+
+})
+
+topHeader.addEventListener('dblclick', (e) => {
+    topHeader.style.fontFamily = 'impact'
+})
+
+//change top nav background color on click
+let count3 = 0;
+let bColors = [
+    'pink', 'orange', 'lightgray', 'teal'
+]
+
+const topNav = document.querySelector('.main-navigation')
+
+topNav.addEventListener('click', (e) => {
+    if(count3 > 3){
+        count3 = 0
+    }
+    e.stopPropagation();
+    topNav.style.backgroundColor = bColors[count3]
+    count3++
+})
+
+//select nav links so that we can stop default and propgation
+const navLinks = document.querySelectorAll('.nav-link')
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) =>{
+        e.stopPropagation()
+        e.preventDefault();
+        paragraphs.forEach(p =>{
+            if(p.style.color === 'blue'){
+                p.style.color = 'red'
+            } else {
+                p.style.color = 'blue'
+            }
+           
+        })
+    })
+})
